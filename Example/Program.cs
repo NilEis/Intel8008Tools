33f98c3c56b3bfe1abb427999ea8a5d4b346fde4
NilEis <52820950+NilEis@users.noreply.github.com>
2024-08-11 22:37:26 +0200

    first version

---
+++ Example/Program.cs
@@ -0,0 +1,5 @@
+﻿using Intel8008Tools;
+
+var cpu = new Intel8008();
+cpu.LoadMemory(@"C:\Users\Nils_Eisenach\Desktop\dev\CS\Intel8008Tools\invaders\invaders.h", 0);
+Console.Out.WriteLine(cpu.Disassemble(0, 0x07ff));
