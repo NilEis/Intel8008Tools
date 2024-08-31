279d05216ff30f2435f28222b4ae964b63a9b790
NilEis <52820950+NilEis@users.noreply.github.com>
2024-08-31 23:11:39 +0200

    add: assembler project

---
+++ Assembler/Assembler.cs
@@ -0,0 +1,47 @@
+﻿namespace Assembler;
+
+using System.CommandLine;
+
+class Assembler
+{
+    static void Main(string[] args)
+    {
+        var rootCmd = new RootCommand("A tool to (dis-)assemble intel 8080 code")
+        {
+            BuildCmd((output, input) => { Console.Out.WriteLine(input); }),
+            DisasmCmd((input) => { Console.Out.WriteLine(input); })
+        };
+        rootCmd.Invoke(args);
+    }
+
+    private static Command DisasmCmd(Action<FileInfo> handler)
+    {
+        var fileArg = new Argument<FileInfo>("bin file", "the binary file to disassemble");
+        var disasmCmd = new Command("dasm", "disassemble a binary file")
+        {
+            fileArg
+        };
+        disasmCmd.AddAlias("disasm");
+        disasmCmd.AddAlias("d");
+        disasmCmd.SetHandler(handler, fileArg);
+        return disasmCmd;
+    }
+
+    private static Command BuildCmd(Action<FileInfo, FileInfo> handler)
+    {
+        var outputOption = new Option<FileInfo>(["-o", "--output"], () => new FileInfo("a.bin"),
+            "the destination for the assembled binary")
+        {
+            IsRequired = false
+        };
+        var fileArg = new Argument<FileInfo>("file", "assembly file");
+        var buildCmd = new Command("build", "assemble an assembler file")
+        {
+            outputOption,
+            fileArg
+        };
+        buildCmd.AddAlias("b");
+        buildCmd.SetHandler(handler, outputOption, fileArg);
+        return buildCmd;
+    }
+}
---
+++ AssemblerBackend/Assembler.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace AssemblerBackend;
+
+public static partial class Assembler
+{
+    public static bool Assemble(string code, [NotNullWhen(true)] out byte[]? buf)
+    {
+        var res = new Queue<byte>();
+        var lines = CommentRegEx().Replace(code, "\n").Split('\n',
+            StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        Console.Out.WriteLine(string.Join('\n', lines));
+        foreach (var line in lines)
+        {
+        }
+
+        buf = res.ToArray();
+        return true;
+    }
+
+    [GeneratedRegex(@";.*$", RegexOptions.Multiline)]
+    private static partial Regex CommentRegEx();
+}
